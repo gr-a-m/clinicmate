220b6efa82755e93c03e32c7b7a40719776d3053
Grant Marshall <grant.a.marshall@asu.edu>
2013-03-24 22:42:55 -0700

    Added junit dependency; Added test stubs for Entity classes

---
+++ test/mavericksoft/clinicmate/PatientTest.java
@@ -0,0 +1,21 @@
+package mavericksoft.clinicmate;
+
+import org.junit.After;
+import org.junit.Before;
+
+/**
+ * This class tests the functionality of the Patient class using JUnit
+ *
+ * @author Grant Marshall
+ */
+public class PatientTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+}

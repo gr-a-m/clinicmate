d958096840b17bf1496ae1dcfd7ac9fd5932e308
unknown <Mark Karlsrud@Mark.(none)>
2013-03-26 16:58:39 -0700

    changed package of gui

--- src/mavericksoft/clinicmate/ClinicMateUI.java
+++ src/mavericksoft/clinicmate/ClinicMateUI.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package mavericksoft.gui;
+package mavericksoft.clinicmate;
 
 /**
  *
--- src/mavericksoft/clinicmate/WelcomeController.java
+++ src/mavericksoft/clinicmate/WelcomeController.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package mavericksoft.gui;
+package mavericksoft.clinicmate;
 
 import java.awt.event.ActionEvent;
 import java.io.IOException;

b47c3159ba6e47c2e0a9bd258eaa3110d37c3a92
unknown <Mark Karlsrud@Mark.(none)>
2013-04-23 22:37:44 -0700

    doctor table of records works now

--- src/mavericksoft/clinicmate/DoctorPage.java
+++ src/mavericksoft/clinicmate/DoctorPage.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.ResourceBundle;
 import java.util.UUID;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -20,9 +21,12 @@ import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.ToggleButton;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 
 /**
@@ -79,16 +83,27 @@ public class DoctorPage implements Initializable {
     private Button logOutButton;
     @FXML
     private ListView<?> patientList;
-    
-    public Patient[] patients;
     @FXML
-    private TableView<?> table;
+    private TableView<Row> table;
     @FXML
     private ToggleButton bloodNonLinToggle;
     @FXML
     private ToggleButton weightNonLinToggle;
     @FXML
     private ToggleButton glucoseNonLinToggle;
+    @FXML
+    private TableColumn<Row,String> dateColumn;
+    @FXML
+    private TableColumn<Row,String> systolicColumn;
+    @FXML
+    private TableColumn<Row,String> diastolicColumn;
+    @FXML
+    private TableColumn<Row,String> glucoseColumn;
+    @FXML
+    private TableColumn<Row,String> weightColumn;
+    
+    private Patient[] patients;
+    private HealthRecord currentRecord;
 
     /**
      * Initializes the controller class.
@@ -107,6 +122,49 @@ public class DoctorPage implements Initializable {
         
         ObservableList items=FXCollections.observableArrayList(patientArray);
         patientList.setItems(items);
+        
+        table.setEditable(false);
+        
+        dateColumn.setCellValueFactory(new PropertyValueFactory<Row,String>("date"));
+        systolicColumn.setCellValueFactory(new PropertyValueFactory<Row,String>("systolic"));
+        diastolicColumn.setCellValueFactory(new PropertyValueFactory<Row,String>("diastolic"));
+        glucoseColumn.setCellValueFactory(new PropertyValueFactory<Row,String>("glucose"));
+        weightColumn.setCellValueFactory(new PropertyValueFactory<Row,String>("weight"));
+        //table.getColumns().setAll(dateColumn,systolicColumn,diastolicColumn,glucoseColumn,weightColumn);
+    }
+    
+    public static class Row
+    {
+        private final SimpleStringProperty date,systolic,diastolic,glucose,weight;
+        private Row(Date date,int systolic,int diastolic,int glucose,int weight)
+        {
+            this.date = new SimpleStringProperty(date.toString());
+            this.systolic = new SimpleStringProperty(Integer.toString(systolic));
+            this.diastolic = new SimpleStringProperty(Integer.toString(diastolic));
+            this.glucose = new SimpleStringProperty(Integer.toString(glucose));
+            this.weight = new SimpleStringProperty(Integer.toString(weight));
+            System.out.println(date.toString() + systolic + diastolic + glucose + weight);
+        }
+        public String getDate()
+        {
+            return date.get();
+        }
+        public String getSystolic()
+        {
+            return systolic.get();
+        }
+        public String getDiastolic()
+        {
+            return diastolic.get();
+        }
+        public String getGlucose()
+        {
+            return glucose.get();
+        }
+        public String getWeight()
+        {
+            return weight.get();
+        }
     }
     
     @FXML
@@ -141,5 +199,31 @@ public class DoctorPage implements Initializable {
     {
         PermissionsController.getInstance().logout();
         new ClinicMatePage("loginPage.fxml",event,"Login");
+    }
+    
+    @FXML
+    public void updateTable(MouseEvent event)
+    {
+        int index=patientList.getSelectionModel().getSelectedIndex();
+        Patient currentPatient=patients[index];
+        
+        ObservableList<DoctorPage.Row> data=FXCollections.observableArrayList();
+        
+        try {
+            HealthRecord[] records=currentPatient.getHealthRecords();
+            
+            if(records!=null)
+            {
+                for(HealthRecord record: records)
+                {
+                    currentRecord=record;
+                    data.add(new Row(record.getDate(),record.getDiaBloodPressure(),record.getSysBloodPressure(),record.getGlucose(),record.getWeight()));
+                }
+            }
+            
+            if(data.isEmpty()){System.out.println("empty data");}
+            table.setItems(data);
+            //ArrayList<String> getComments();
+        }catch(Exception ex){System.out.println("exception");}
     }
 }
